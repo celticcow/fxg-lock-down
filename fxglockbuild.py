9b787bb051337035cfe6352e2b55217a43dca61d
Celticcow <gregory.dunlap@gmail.com>
2020-07-08 07:58:57 -0500

    http setup

--- fxglockbuild.py
+++ fxglockbuild.py
@@ -1,4 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 -W ignore::DeprecationWarning
+
+import apifunctions
+
+#remove the InsecureRequestWarning messages
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
 
 ##
 ##
